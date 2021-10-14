0e323c0e2f63307c2d9e98716bd896a658070438
MrCurio <juanplatero98@gmail.com>
2021-10-14 10:56:07 +0200

    Light game v1

--- juan_sdp/Tarea1/lights_game.v
+++ juan_sdp/Tarea1/lights_game.v
@@ -1,17 +1,44 @@
-module lights_game(clk, reset, enable, up_down, count, TC);
+module lights_game(clk, reset, enable, up_down, leds);
 
-parameter end_count = 16; //Maximum number to count to
+//parameter end_count = 12500000; //Maximum number to count to
+parameter end_count = 3; //Maximum number to count to
 
 `include "MathFun.vh"
 
 parameter N = CLogB2(end_count - 1);
 
 input clk, reset, enable, up_down;
-output [N-1:0] count;
-output TC;
+output reg [7:0] leds;
 
+wire [N-1:0] count;
+wire TC, shift_in, shift_out;
+wire [13:0] PR;
 
-n_counter #(end_count) counter_16 (clk, reset, enable, up_down, count, TC);
+assign shift_in = ~shift_out;
+
+n_counter #(end_count) counter_leds (clk, reset, enable, up_down, count, TC);
+
+n_shift_reg #(14) reg_14 (clk, reset, TC, shift_in, PR, shift_out);
+
+always @(posedge clk)
+case (PR)
+    14'b10000000000000 || ~(14'b10000000000000): leds = 8'b10000000; 
+    14'b11000000000000 : leds = 8'b01000000; 
+    14'b11100000000000 : leds = 8'b00100000; 
+    14'b11110000000000 : leds = 8'b00010000; 
+    14'b11111000000000 : leds = 8'b00001000; 
+    14'b11111100000000 : leds = 8'b00000100; 
+    14'b11111110000000 : leds = 8'b00000010; 
+    14'b11111111000000 : leds = 8'b00000001; 
+
+    14'b11111111100000 : leds = 8'b00000010; 
+    14'b11111111110000 : leds = 8'b00000100; 
+    14'b11111111111000 : leds = 8'b00001000; 
+    14'b11111111111100 : leds = 8'b00010000; 
+    14'b11111111111110 : leds = 8'b00100000; 
+    14'b11111111111111 : leds = 8'b01000000; 
+    default: leds = 8'b11111111;
+endcase
 
 endmodule
 
--- juan_sdp/Tarea1/simulation/qsim/lights_game.do
+++ juan_sdp/Tarea1/simulation/qsim/lights_game.do
@@ -1,7 +1,7 @@
 onerror {exit -code 1}
 vlib work
 vlog -work work lights_game.vo
-vlog -work work n_counter_simvwf.vwf.vt
+vlog -work work Waveform.vwf.vt
 vsim -novopt -c -t 1ps -L cycloneive_ver -L altera_ver -L altera_mf_ver -L 220model_ver -L sgate_ver -L altera_lnsim_ver work.lights_game_vlg_vec_tst
 vcd file -direction lights_game.msim.vcd
 vcd add -internal lights_game_vlg_vec_tst/*
@@ -15,3 +15,4 @@ proc simTimestamp {} {
 after 2500 simTimestamp
 run -all
 quit -f
+
