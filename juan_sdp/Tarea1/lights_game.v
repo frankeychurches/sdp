f1f8d5bbf9f28003155e33d1c5179a9ffa4ba6d1
MrCurio <juanplatero98@gmail.com>
2021-10-14 12:30:48 +0200

    Tarea 1 v1.1

--- juan_sdp/Tarea1/lights_game.v
+++ juan_sdp/Tarea1/lights_game.v
@@ -8,7 +8,7 @@ parameter end_count = 4; //Maximum number to count to
 parameter N = CLogB2(end_count - 1);
 
 input clk, reset, enable, up_down;
-output reg [7:0] leds;
+output [7:0] leds;
 
 wire [N-1:0] count;
 wire TC, shift_in, shift_out;
@@ -20,25 +20,14 @@ n_counter #(end_count) counter_leds (clk, reset, enable, up_down, count, TC);
 
 n_shift_reg #(14) reg_14 (clk, reset, TC, shift_in, PR, shift_out);
 
-always @(posedge clk)
-case (PR)
-    14'b10000000000000 || ~(14'b10000000000000): leds = 8'b10000000; 
-    14'b11000000000000 : leds = 8'b01000000; 
-    14'b11100000000000 : leds = 8'b00100000; 
-    14'b11110000000000 : leds = 8'b00010000; 
-    14'b11111000000000 : leds = 8'b00001000; 
-    14'b11111100000000 : leds = 8'b00000100; 
-    14'b11111110000000 : leds = 8'b00000010; 
-    14'b11111111000000 : leds = 8'b00000001; 
-
-    14'b11111111100000 : leds = 8'b00000010; 
-    14'b11111111110000 : leds = 8'b00000100; 
-    14'b11111111111000 : leds = 8'b00001000; 
-    14'b11111111111100 : leds = 8'b00010000; 
-    14'b11111111111110 : leds = 8'b00100000; 
-    14'b11111111111111 : leds = 8'b01000000; 
-    default: leds = 8'b00000000;
-endcase
+assign leds[7] = (PR == 14'b10000000000000 || PR == ~14'b10000000000000) ? 1 : 0;
+assign leds[6] = (PR == 14'b11000000000000 || PR == ~14'b11000000000000) ? 1 : (PR == 14'b11111111111111) ? 1 : 0;
+assign leds[5] = (PR == 14'b11100000000000 || PR == ~14'b11100000000000) ? 1 : (PR == 14'b11111111111110) ? 1 : 0;
+assign leds[4] = (PR == 14'b11110000000000 || PR == ~14'b11110000000000) ? 1 : (PR == 14'b11111111111100) ? 1 : 0;
+assign leds[3] = (PR == 14'b11111000000000 || PR == ~14'b11111000000000) ? 1 : (PR == 14'b11111111111000) ? 1 : 0;
+assign leds[2] = (PR == 14'b11111100000000 || PR == ~14'b11111100000000) ? 1 : (PR == 14'b11111111110000) ? 1 : 0;
+assign leds[1] = (PR == 14'b11111110000000 || PR == ~14'b11111110000000) ? 1 : (PR == 14'b11111111100000) ? 1 : 0;
+assign leds[0] = (PR == 14'b11111111000000 || PR == ~14'b11111111000000) ? 1 : 0;
 
 //Tengo que usar un assign porque el registro usa flip flops y va retrasado una unidad 
 
